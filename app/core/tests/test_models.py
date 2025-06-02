3b6d75c749e9bcb30ca326cb128c74502a2f7ca6
Ravindra Sham Pawar <Ravindra.Pawar@anyonehome.com>
2025-06-02 22:18:09 +0530

    Added superuser support

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -45,3 +45,13 @@ class ModelTests(TestCase):
                 " ",
                 "test123"
             )
+
+    def test_create_superuser(self):
+        """Test creating super user."""
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "test@123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
