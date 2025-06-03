7f01e778d1318d1d5d268de22f45c7cad61b33e5
Ravindra Sham Pawar <Ravindra.Pawar@anyonehome.com>
2025-06-03 13:15:21 +0530

    Added edit user to admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,6 +5,7 @@ Django admin customization.
 
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from core import models
 
 
@@ -12,6 +13,31 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for user."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (
+            None,
+            {
+               'fields': ('email', 'password',)
+            }
+        ),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_superuser',
+                    'is_staff',
+                )
+            }
+        ),
+        (
+            _('Important Dates'),
+            {
+                "fields": ("last_login",)
+            }
+        )
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
